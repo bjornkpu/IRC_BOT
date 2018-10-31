41cea3b4cc0273f61a9ec09604f78b85d389e5ef
Bjørn Kristian Punsvik <bk.punsvik@gmail.com>
2018-10-31 10:30:04 +0100

    added group listing and help, changes to tihlde-drift form test channel

--- IRC_BOT.py
+++ IRC_BOT.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import socket
 import json
+import getpass
 
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server = "chat.freenode.net" # Server
-channel = "#bot-test-bk" # Channel
-botnick = "huleBOT" # Your bots nick
+channel = "#tihlde-drift" # Channel
+botnick = "mentionbot" # Your bots nick
 adminname = "bjornkpu" #Your IRC nickname
 exitcode = "bye " + botnick
 
@@ -16,7 +17,8 @@ ircsock.send(bytes("USER "+ botnick +" "+ botnick +" "+ botnick + " " + botnick 
 ircsock.send(bytes("NICK "+ botnick +"\n", "UTF-8")) # assign the nick to the bot
 
 def joinchan(chan): # join channel(s).
-    ircsock.send(bytes("JOIN "+ chan +"\n", "UTF-8"))
+    pw = getpass.getpass("Password for " + chan + ": ")
+    ircsock.send(bytes("JOIN "+ chan + " " + pw + "\n", "UTF-8"))
     ircmsg = ""
     while ircmsg.find("End of /NAMES list.") == -1:
         ircmsg = ircsock.recv(2048).decode("UTF-8")
@@ -27,7 +29,7 @@ def ping(): # respond to server Pings.
     ircsock.send(bytes("PONG :pingis\n", "UTF-8"))
 
 def sendmsg(msg, target=channel): # sends messages to the target.
-    ircsock.send(bytes("PRIVMSG "+ target +" :"+ msg +"\n", "UTF-8"))
+    ircsock.send(bytes("NOTICE "+ target +" :"+ msg +"\n", "UTF-8"))
 
 def readJson():
     with open("data_file.json", "r") as read_file:
@@ -78,19 +80,25 @@ def main():
                     sendmsg(message, target)
 
                 # Group management
-                #if message[:5].find('@groups') != -1:
-                    #grouplist = ""
-                    #for group in groups:
-
                 if message[:5].find('@') != -1:
                     command = message.split(' ')
                     if len(command) == 1:
                         group = message[1:].split(' ',3)[0]
-                        if group in groups:
-                            userlist = ""
-                            for user in groups[group]:
-                                userlist += user + ":"
-                            print(userlist)
+                        if group == botnick:
+                            sendmsg("@" + botnick + "                    - Help.")
+                            sendmsg("@groups                        - List groups.")
+                            sendmsg("@<group> <add/remove> <user>   - Add/remove a given user.")
+                        if group == "groups":
+                            grouplist = ""
+                            for group in groups:
+                                grouplist += group + " "
+                            sendmsg(grouplist)
+                        else:
+                            if group in groups:
+                                userlist = ""
+                                for user in groups[group]:
+                                    userlist += user + ":"
+                                sendmsg(userlist)
 
                     if len(command) == 3 and message[1:].split(' ',3)[0] in groups:
                         if message.split(' ',3)[1].lower() == "add":
